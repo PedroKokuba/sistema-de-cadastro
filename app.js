d26c887eee927419251889cb59ec23329bbb3a57
PedroKokuba <pedro.kokuba@gmail.com>
2023-04-20 10:13:12 -0300

    Testando o git restore

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos!")
 console.log("Aplicação está se conectando ao banco de dados!")
+
+//Teste para git restore
+console.log("Testando o git restore")
