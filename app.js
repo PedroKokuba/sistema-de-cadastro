fd51b852834dd41e2c7ceab774cb29beafa5e70a
PedroKokuba <pedro.kokuba@gmail.com>
2023-04-20 10:16:57 -0300

    Voltando para antes de testar o git restore

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos!")
 console.log("Aplicação está se conectando ao banco de dados!")
-
-//Teste para git restore
-console.log("Testando o git restore")
